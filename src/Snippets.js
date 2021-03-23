cba585cb2adf9fd3431003e8c47289642e609506
WEBSTER Christopher <Christopher.Webster@education.wa.edu.au>
2021-03-23 15:34:13 +0800

    added tagging server as arguement

--- src/Snippets.js
+++ src/Snippets.js
@@ -3,12 +3,12 @@ import warn from './utils/warn'
 // https://developers.google.com/tag-manager/quickstart
 
 const Snippets = {
-  tags: function ({ id, events, dataLayer, dataLayerName, preview, auth }) {
+  tags: function ({ id, gtm_server, events, dataLayer, dataLayerName, preview, auth }) {
     const gtm_auth = `&gtm_auth=${auth}`
     const gtm_preview = `&gtm_preview=${preview}`
 
     if (!id) warn('GTM Id is required')
-    if (!gtm_server) warn('Tagging Server is required')
+    if (!gtm_server) {const gtm_server = "www.googletagmanager.com"}
 
     const iframe = `
       <iframe src="https://${gtm_server}/ns.html?id=${id}${gtm_auth}${gtm_preview}&gtm_cookies_win=x"
